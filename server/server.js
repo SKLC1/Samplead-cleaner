b4c9820a027215110305405accee8bde58dc5967
SKLC1 <segevklein2000@gmail.com>
2022-09-01 09:52:40 +0300

    heroku

--- server/server.js
+++ server/server.js
@@ -29,6 +29,12 @@ app.use(express.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 app.use('/resume', resumeRouter)
 
+app.use(function (req, res) {
+  var delayed = new DelayedResponse(req, res);
+  // verySlowFunction can now run indefinitely
+  scrapePostsV2(delayed.start());
+});
+
 app.listen(PORT,()=>{
   console.log(`server running on ${PORT}`);
 });
